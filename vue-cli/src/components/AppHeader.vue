a5485e25c2def3bd678fab5adb57ebee25db1d6e
gunny6026 <gunny6026@naver.com>
2021-01-28 10:54:41 +0900

    vue 씹어먹자!!!

---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+      <h1> {{propsdata}}</h1>
+      <button v-on:click="sendEvent">전송</button>
+    
+</div>
+</template>
+
+<script>
+export default {
+    props: ['propsdata'],
+    methods:{
+        sendEvent : function(){
+            this.$emit('renew');
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-form/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- form의 기본 동작은 새로고침이다.  -->
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username"> id: </label>
+      <input type="text" id="username" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw:</label>
+      <input type="text" id="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data : function(){
+    return{
+      username: '',
+      password: '',
+
+    }
+    
+  },
+  methods: {
+      submitForm : function(){
+        // event.preventDefault(); 일반 자바스크립트 , 제이쿼리에서 사용함 
+        console.log(this.username, this.password);
+
+      }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
